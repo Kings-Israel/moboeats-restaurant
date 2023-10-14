4670f2a15c2cabbc1d9f04f5efe95a5d9f38dca0
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-10-14 12:52:14 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: /^~.+/,
+        replacement: (val) => {
+          return val.replace(/^~/, "");
+        },
+      },
+    ],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  },
+  server: {
+    host: 'restaurant.moboeats.test',
+    port: 3000
+  }
+})
