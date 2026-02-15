e78a824b68ae4b550174854b50864acef4b08b2d
Kings-Israel <milimokings@gmail.com>
2026-02-15 14:22:09 +0300

    Updated UI

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import router from '../router'
 import { useToast } from 'vue-toastification'
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://api.moboeats.test/api/v1/' : 'https://ecodeli.backend.amaniaccess.com/api/v1'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://api.moboeats.test/api/v1/' : 'https://javahouse.backend.amaniaccess.com/api/v1'
 
 const toast = useToast()
 
